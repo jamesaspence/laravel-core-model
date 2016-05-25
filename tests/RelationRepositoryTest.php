3db1100d4c170ca60706147ae3ac7e4ca0ae980f
James Spence <Jamesaspence20@gmail.com>
2016-05-25 13:22:29 -0500

    finished RelationRepository tests

--- tests/RelationRepositoryTest.php
+++ tests/RelationRepositoryTest.php
@@ -8,13 +8,16 @@
 
 namespace Tests;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Laracore\Repository\Relation\RelationRepository;
 use Illuminate\Database\Eloquent\Model;
+use Mockery\Mock;
 
 class RelationRepositoryTest extends TestCase
 {
     /**
-     * @var RelationRepository
+     * @var RelationRepository|Mock
      */
     protected $relationRepository;
 
@@ -22,9 +25,7 @@ class RelationRepositoryTest extends TestCase
     {
         parent::setUp();
 
-//        $repository = \Mockery::mock(RelationRepository::class);
-
-        $repository = new RelationRepository();
+        $repository = \Mockery::mock(RelationRepository::class)->makePartial();
 
         $this->relationRepository = $repository;
     }
@@ -32,6 +33,11 @@ class RelationRepositoryTest extends TestCase
     public function createMockModel()
     {
         return \Mockery::mock(Model::class);
+    }
+
+    public function createMockRelation()
+    {
+        return \Mockery::mock(BelongsTo::class);
     }
 
     public function testSetRelation()
@@ -71,86 +77,194 @@ class RelationRepositoryTest extends TestCase
 
     public function testAssociateRelation()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $model = $this->createMockModel();
+        $relation = 'relation';
+        $value = $this->createMockModel();
+
+        $mockRelation = \Mockery::mock(BelongsTo::class);
+        $mockRelation
+            ->shouldReceive('associate')
+            ->with($value)
+            ->once()
+            ->andReturn($model);
+
+        $model
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation);
+
+        $this->relationRepository->associateRelation($model, $relation, $value);
     }
 
     public function testAssociateMany()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $model = $this->createMockModel();
+
+        $relations = [
+            'relation' => $this->createMockModel(),
+            'otherRelation' => $this->createMockModel()
+        ];
+
+        foreach ($relations as $relation => $value) {
+            $this
+                ->relationRepository
+                ->shouldReceive('associateRelation')
+                ->with($model, $relation, $value)
+                ->andReturn($model);
+        };
+
+        $this->relationRepository->associateMany($model, $relations);
     }
 
     public function testDissociateRelation()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $mockRelation = $this->createMockRelation();
+        $mockRelation
+            ->shouldReceive('dissociate')
+            ->once();
+
+        $model = $this->createMockModel();
+        $model
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation);
+
+        $this->relationRepository->dissociateRelation($model, $relation);
     }
 
     public function testAttachRelation()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $modelId = 1;
+        $tableAttributes = ['stuff' => 'things'];
+        $mockRelation = $this->createMockRelation();
+        $mockRelation
+            ->shouldReceive('attach')
+            ->with($modelId, $tableAttributes)
+            ->once();
+
+        $model = $this->createMockModel();
+        $model
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation);
+
+        $this->relationRepository->attachRelation($model, $relation, $modelId, $tableAttributes);
     }
 
     public function testDetachRelation()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $modelId = 1;
+        $mockRelation = $this->createMockRelation();
+        $mockRelation
+            ->shouldReceive('detach')
+            ->with($modelId)
+            ->once();
+
+        $model = $this->createMockModel();
+        $model
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation);
+
+        $this->relationRepository->detachRelation($model, $relation, $modelId);
     }
 
     public function testUpdateExistingPivot()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $id = 1;
+        $tableAttributes = ['stuff' => 'things'];
+        $mockRelation = $this->createMockRelation();
+        $mockRelation
+            ->shouldReceive('updateExistingPivot')
+            ->with($id, $tableAttributes)
+            ->once();
+
+        $model = $this->createMockModel();
+        $model
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation);
+
+        $this->relationRepository->updateExistingPivot($model, $relation, $id, $tableAttributes);
     }
 
     public function testSync()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $ids = [1, 2];
+        $mockRelation = $this
+            ->createMockRelation()
+            ->shouldReceive('sync')
+            ->with($ids)
+            ->once()
+            ->getMock();
+
+        $model = $this
+            ->createMockModel()
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation)
+            ->getMock();
+
+        $this->relationRepository->sync($model, $relation, $ids);
     }
 
     public function testSaveMany()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $value = $this->createMockModel();
+        $mockRelation = $this
+            ->createMockRelation()
+            ->shouldReceive('saveMany')
+            ->with($value)
+            ->once()
+            ->getMock();
+
+        $model = $this
+            ->createMockModel()
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation)
+            ->getMock();
+
+        $this->relationRepository->saveMany($model, $relation, $value);
     }
 
     public function testSave()
     {
-        $this->stub();
-        //TODO fill in test contents.
+        $relation = 'relation';
+        $value = $this->createMockModel();
+        $tableAttributes = ['stuff' => 'things'];
+        $mockRelation = $this
+            ->createMockRelation()
+            ->shouldReceive('save')
+            ->with($value, $tableAttributes)
+            ->once()
+            ->getMock();
+
+        $model = $this
+            ->createMockModel()
+            ->shouldReceive($relation)
+            ->once()
+            ->andReturn($mockRelation)
+            ->getMock();
+
+        $this->relationRepository->save($model, $relation, $value, $tableAttributes);
     }
 
     public function testRelationsIsA()
     {
-        $this->stub();
-        //TODO fill in test contents.
-    }
+        $relation = \Mockery::mock(Relation::class);
+        $expected = Relation::class;
 
-    public function testRelationIsBelongsTo()
-    {
-        $this->stub();
-        //TODO fill in test contents.
-    }
+        $this->assertTrue($this->relationRepository->relationsIsA($relation, $expected));
 
-    public function testRelationsIsBelongsToMany()
-    {
-        $this->stub();
-        //TODO fill in test contents.
-    }
-
-    public function testRelationIsHasOne()
-    {
-        $this->stub();
-        //TODO fill in test contents.
-    }
-
-    public function testRelationIsHasMany()
-    {
-        $this->stub();
-        //TODO fill in test contents.
+        //Test for failure too
+        $this->assertFalse($this->relationRepository->relationsIsA($relation, BelongsTo::class));
     }
 
     public function tearDown()
