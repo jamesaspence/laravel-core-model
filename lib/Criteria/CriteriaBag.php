ee1a4971aa66774527e018897b882ae235fa354b
James Spence <Jamesaspence20@gmail.com>
2016-08-16 23:20:02 -0500

    Criteria and CriteriaBag

---
+++ lib/Criteria/CriteriaBag.php
@@ -0,0 +1,99 @@
+<?php
+
+
+namespace Laracore\Criteria;
+
+
+class CriteriaBag
+{
+    /**
+     * @var CriteriaInterface[]
+     */
+    protected $criteria;
+
+    /**
+     * Adds a criteria to the bag.
+     *
+     * @param CriteriaInterface $criteria
+     */
+    public function add(CriteriaInterface $criteria)
+    {
+        $this->criteria[] = $criteria;
+    }
+
+    /**
+     * Retrieves all criteria.
+     *
+     * @return CriteriaInterface[]
+     */
+    public function all()
+    {
+        return $this->criteria;
+    }
+
+    /**
+     * Applies all criteria.
+     *
+     * @param $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function applyAll($model)
+    {
+        /** @var CriteriaInterface $criteria */
+        foreach ($this->criteria as $criteria) {
+            $model = $criteria->apply($model);
+        }
+
+        return $model;
+    }
+
+    /**
+     * Clears the criteria.
+     *
+     * @return static
+     */
+    public function clear()
+    {
+       $this->criteria = [];
+
+        return $this;
+    }
+
+    /**
+     * Clears non-persistent criteria.
+     *
+     * @return static
+     */
+    public function clearNonPersistent()
+    {
+        $newCriteria = [];
+        foreach ($this->criteria as $criteria) {
+            if ($criteria->isPersistent()) {
+                $newCriteria[] = $criteria;
+            }
+        }
+
+        $this->criteria = $newCriteria;
+
+        return $this;
+    }
+
+    /**
+     * Checks if there are criteria to apply.
+     *
+     * @return bool
+     */
+    public function criteriaToApply()
+    {
+        $count = 0;
+
+        foreach ($this->criteria as $criteria) {
+            if (!$criteria->shouldBeSkipped()) {
+                ++$count;
+            }
+        }
+
+        return $count;
+    }
+
+}
